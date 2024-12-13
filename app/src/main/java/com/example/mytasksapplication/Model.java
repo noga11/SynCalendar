f5ca64ed673fb5fbf3b292e93b7c5de38075e7e0
Admin <noga.golomb.haalman@gmail.com>
2024-12-13 11:37:29 +0200

    Firebase - connected Model - started

--- app/src/main/java/com/example/mytasksapplication/Model.java
+++ app/src/main/java/com/example/mytasksapplication/Model.java
@@ -1,4 +1,17 @@
 package com.example.mytasksapplication;
 
+import android.content.Context;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Model {
+    private static Model instance;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference dbUsers = db.collection("Users");
+    private Context context;
+    private User currentUser;
+
 }
