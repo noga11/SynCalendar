344139db941dc075c9d52729ffdde58720e06d2b
Admin <noga.golomb.haalman@gmail.com>
2025-04-29 12:29:19 +0300

    calendar shows

--- app/src/main/java/com/example/SynCalendar/CustomCalendar/CalendarAdapter.java
+++ app/src/main/java/com/example/SynCalendar/CustomCalendar/CalendarAdapter.java
@@ -30,6 +30,7 @@ public class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
         View view = inflater.inflate(R.layout.calendar_cell, parent, false);
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        view.setLayoutParams(layoutParams);
         return new CalendarViewHolder(view, onItemListener);
     }
 
